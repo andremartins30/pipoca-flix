dad2c2bcc1ce0ac9f751265492c5b6a37661746e
andremartins30 <87194533+andremartins30@users.noreply.github.com>
2023-07-06 23:13:27 -0400

    Melhorias Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './header.css'
 import {Link} from 'react-router-dom'
+import icon from './icon.png'
+
 
 
 const Header = () => {
     return (
         <header>
-            <Link className='logo' to='/'>PipocaFLIX</Link>
+            <Link className='logo' to='/'><img src={icon} alt='icon' />PipocaFLIX</Link>
             <Link className='favoritos' to='/favoritos'>Meus Filmes</Link>
         </header>
     )
